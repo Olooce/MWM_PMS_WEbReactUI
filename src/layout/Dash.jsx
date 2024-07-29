3c14d547ce6b9a8d4f516c20b78af3de76c5a98a
Olooce <asirastephen9@gmail.com>
2024-07-29 09:40:01 +0300

    Update

--- src/layout/Dash.jsx
+++ src/layout/Dash.jsx
@@ -59,7 +59,7 @@ const Dash = ({ children }) => {
                     </button>
                 </div>
                 <div className="navbar-left">
-                    <img src="../assets/logo-no-background.svg" alt="Logo" className="logo" />
+                    <img src="src/assets/logo-no-background.svg"  className="logo" />
                 </div>
                 <div className="navbar-center">
                     {/* Search bar could go here */}
