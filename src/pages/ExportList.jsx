30b98f1aed22c93ba1f6e8646fc95218249d7def
Olooce <asirastephen9@gmail.com>
2024-07-30 10:00:01 +0300

    Updates

--- src/pages/ExportList.jsx
+++ src/pages/ExportList.jsx
@@ -6,13 +6,21 @@ import ExportsTable from './ExportsTable';
 import Pagination from '../components/Pagination';
 
 export default function{
-    const [exports, setSalaries] = useState([]);
+    const [exports, setExports] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [page, setPage] = useState(1);
     const [size, setSize] = useState(10);
     const [inputPage, setInputPage] = useState(1);
     const [inputSize, setInputSize] = useState(10);
+    const response,data;
 
-    const fetchExports = useCallback(() => {});
+    const fetchExports = useCallback(async () => {
+        try {
+            setLoading(true);
+            response = await getExports(page,size);
+            data = response.data;
+            setExports(Array.isArray(data) ? data : [];)           
+        }
+    });
 }
