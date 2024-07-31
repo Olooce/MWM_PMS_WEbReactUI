2028a17804b93d4949f6233304ed3b73ac9837cc
Olooce <asirastephen9@gmail.com>
2024-07-31 13:10:01 +0300

    Updates

--- src/components/tables/SalariesTable.jsx
+++ src/components/tables/SalariesTable.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-
-// const SalariesTable = ({ salaries, page, size }) => (
-//   <div className="table-wrapper">
-//     <table className="fl-table">
-//       <thead>
-//         <tr>
-//           <th>#</th>
-//           <th>EmployeeID</th>
-//           <th>Date</th>
-//           <th>NetSalary</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {salaries.map((salary, index) => (
-//           <tr key={salary.salaryId}>
-//             <td>{(page - 1) * size + index + 1}</td>
-//             <td>{salary.employeeId}</td>
-//             <td>{salary.month}</td>
-//             <td>{salary.netSalary}</td>
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   </div>
-// );
-
-// export default SalariesTable;
-
-
 import React from 'react';
 import Table from '../../components/Table';
 
