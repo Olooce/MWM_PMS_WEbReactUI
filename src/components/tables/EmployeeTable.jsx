66570e0c92d7bbe538007faec0a19cd9bac696b9
Olooce <asirastephen9@gmail.com>
2024-08-01 23:50:01 +0300

    Update

--- src/components/tables/EmployeeTable.jsx
+++ src/components/tables/EmployeeTable.jsx
@@ -1,37 +1,3 @@
-// import React from 'react';
-
-// const EmployeeTable = ({ employees, page, size, showEmployeeDetails }) => (
-//     <div className="table-wrapper">
-//         <table className="fl-table">
-//             <thead>
-//                 <tr>
-//                     <th>#</th>
-//                     <th>Name</th>
-//                     <th>Department ID</th>
-//                     <th>Employment Type</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {employees.map((employee, index) => (
-//                     <tr key={employee.employeeId}>
-//                         <td>
-//                             <button className="cell-btn" onClick={() => showEmployeeDetails(employee)}>
-//                                 {(page - 1) * size + index + 1}
-//                             </button>
-//                         </td>
-//                         <td>{employee.name}</td>
-//                         <td>{employee.departmentId}</td>
-//                         <td>{employee.employmentType}</td>
-//                     </tr>
-//                 ))}
-//             </tbody>
-//         </table>
-//     </div>
-// );
-
-// export default EmployeeTable;
-
-
 import React from 'react';
 import Table from '../Table';
 
