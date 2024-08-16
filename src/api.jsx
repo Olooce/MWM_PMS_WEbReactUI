85dd871d36916fe50dbc6ab628a407305dd00481
Olooce <asirastephen9@gmail.com>
2024-08-16 10:10:01 +0300

    More updates

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://localhost:8080',
-  withCredentials: false,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
